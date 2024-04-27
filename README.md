b988f5d685b51374a1d24b51707afb0a1d10a496
Matheus Santos <80715832+matheus412@users.noreply.github.com>
2024-04-27 15:30:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EEF-Marina-Ximenes-Frota
+ 
---
+++ src/script.js
@@ -0,0 +1,49 @@
+
+    /* menu-burguer */
+function toggleMenu() {
+    var navMobile = document.getElementById("nav-mobile");
+    var menuIcon = document.getElementById("menu-mobile").getElementsByTagName("img")[0];
+
+    if (navMobile.style.display === "block") {
+        navMobile.style.display = "none";
+        menuIcon.src = "imagens/icon-hamburger.svg";
+        
+    } else {
+        navMobile.style.display = "block";
+        menuIcon.src = "imagens/icon-close.svg";
+    }
+}
+
+let imagemAtual = 0;
+const imagens = document.querySelectorAll("#acontece-fotos .imagem-container");
+const intervalo = 5000; 
+
+function mostrarImagem(index) {
+    imagens.forEach((imagem, i) => {
+        if (i === index) {
+            imagem.style.display = "block";
+        } else {
+            imagem.style.display = "none";
+        }
+    });
+}
+
+function mudarImagem(delta) {
+    imagemAtual += delta;
+    if (imagemAtual < 0) {
+        imagemAtual = imagens.length - 1;
+    } else if (imagemAtual >= imagens.length) {
+        imagemAtual = 0;
+    }
+    mostrarImagem(imagemAtual);
+}
+
+function avancarAutomaticamente() {
+    setInterval(() => {
+        mudarImagem(1);
+    }, intervalo);
+}
+
+mostrarImagem(imagemAtual);
+avancarAutomaticamente();
+
